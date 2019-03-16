0d6bf77ddafc2e9eaf81140f6b53c581e365608f
Lahloug <ghali-lahlou998@hotmail.com>
2019-03-16 19:14:52 +0100

    make initial 2048 board

--- game.c
+++ game.c
@@ -15,6 +15,58 @@ void new_game_board(short (*board)[4])
 	}
 }
 
+void add_random_tile(short tile, short (*board)[4])
+{
+	// Finding empty cells
+	int i, j, n;
+	int empty_cells[16][2];
+	int n_empty_cells = 0;
+	for (i=0; i < 4; i++)
+	{
+		for (j=0; j<4; j++)
+		{
+			if (board[i][j] == 0)
+			{
+				empty_cells[n_empty_cells][0] = i;
+				empty_cells[n_empty_cells][1] = j;
+				n_empty_cells ++;
+			}
+		}
+	}
+	if (n_empty_cells == 0)
+	{
+		printf("No empty cell found!");
+	}
+	n = (rand() % n_empty_cells);
+	i = empty_cells[n][0];
+	j = empty_cells[n][1];
+	if (board[i][j] != 0)
+	{
+		printf("Overwriting cell!");
+	}
+	board[i][j] = tile;
+}
+
+
+void add_random_tile_start(short (*board[4]))
+{
+	// if a tile value is 2^x, we represent it as x
+	// 20 % of the time a 4 tile and 2 tile
+	// 80 % of the time two 2 tile
+	int p;
+	p = (rand() % 10) +1;
+	int tile1 = 1;
+	if (p <= 2)
+	{
+		int tile2 = 4;
+	}
+	else
+	{
+		int tile2 = 2;
+	}
+	// Fill random location
+}
+
 void
 print_board(short (*board)[4])
 {
--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 int main()
 {
 	short board[4][4];
+	short tile = 2;
 	new_game_board(board);
+	add_random_tile(tile, board);
 	print_board(board);
 	return 0;
 }
